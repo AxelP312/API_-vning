d8287741abc0a0e08395123bd7440f4505b9d9fa
AxelP312 <60465685+AxelP312@users.noreply.github.com>
2021-02-17 10:47:26 +0100

    Kommentarer + Fråga om pokemon

---
+++ API_Övning/Pokemon.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace API_Övning
+{
+    public class Pokemon
+    {
+        public string name;
+        public string height;
+        public string weight;
+    }
+}
--- API_Övning/Program.cs
+++ API_Övning/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using RestSharp;
+using Newtonsoft.Json;
 
 namespace API_Övning
 {
@@ -7,6 +9,39 @@ namespace API_Övning
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            //Skapar en klient som kan anropa en api
+            RestClient klient = new RestClient("https://pokeapi.co/api/v2/");
+
+            //Frågar om man 
+            Console.WriteLine("Vilken pokemon vill du söka upp?");
+            Console.Write("");
+            string svarPokemon = Console.ReadLine();
+
+            //Skapar ett objekt som kan fråga om information
+            RestRequest förfrågan = new RestRequest("pokemon/" + svarPokemon);
+
+            //Objekt som kan ta emot det svar man frågar efter. 
+            IRestResponse svar = klient.Get(förfrågan);
+
+            //String som innehåller det svar man frågar efter.
+            string innehåll = svar.Content;
+
+            
+            // Console.WriteLine(innehåll);
+            // Console.ReadLine();
+            //kollar om det gick igenom
+
+            //Skapar en pokemon efter informationen i JSON-filen samt klassen.
+            Pokemon pokemon = JsonConvert.DeserializeObject<Pokemon>(innehåll);
+
+            System.Console.WriteLine(pokemon.height);
+            Console.ReadLine();
+
+
+
+
+
         }
     }
 }
